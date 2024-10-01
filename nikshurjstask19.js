d3562aa259808804a1622aeb466cf9be5e27a1dc
FR1K676 <152060296+FR1K676@users.noreply.github.com>
2024-10-01 18:49:13 +0300

    Add files via upload

---
+++ nikshurjstask19.js
@@ -0,0 +1,46 @@
+/*task22_1*/
+let a=1.5;
+let b=0.75;
+alert(a+b);
+/*task23_1*/
+a=-100;
+alert(a);
+/*task23_2*/
+a=666;
+alert(-a);
+/*task25_1*/
+a=13;
+b=5;
+alert(a%b);
+/*task26_1*/
+a=2;
+b=10;
+alert(a**b);
+/*task28_1*/
+let str="Shurygin";
+alert(str);
+/*task29_1*/
+str="!!!"
+alert(str);
+/*task29_2*/
+let stri="java";
+let strin="script";
+alert(stri+strin);
+/*task29_3*/
+let string="hello";
+let stringi="world";
+alert(string+stringi);
+/*task30_1*/
+let len="jnigdfijndfgdfgjnidfgjniedfgnijjnidfgjidfgnijndfgjniodfgjniodfgjinodfgijnoojnik";
+alert(len.length);
+/*task31_1*/
+let str1='xxx';
+let str2='yyy';
+let txt='aaa '+str1+' bbb '+str2+' ccc';
+alert(txt);
+/*task34_1*/
+let ab;
+alert(ab);
+/*task35_1*/
+let bc=null;
+alert(bc);
