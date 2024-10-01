d1464046f8e008af08f72ef85ceb4ebb6b8c58b5
FR1K676 <152060296+FR1K676@users.noreply.github.com>
2024-10-01 18:18:00 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+"use strict";//strogiy rejim
+let num='Конец проги';
+let a;
+a=10
+alert(a)
+a=20;
+alert(a);
+a=1+2+3;
+alert(a);
+a=10;
+let b=2;
+alert(a+b);
+
+alert(a-b);
+
+alert(a*b);
+
+alert(a/b);
+let c=10;
+let d=5;
+let result=c+d;
+alert(result);
+a=1;
+b=2;
+c=3;
+alert(a+b+c);
+alert(num);/*alert*/
+
