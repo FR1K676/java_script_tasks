9950c62eed8ea16b2f331d9d46b5174e3f4a0b0f
FR1K676 <152060296+FR1K676@users.noreply.github.com>
2024-10-21 16:24:06 +0300

    Add files via upload

---
+++ nikshurjstask19.js
@@ -0,0 +1,116 @@
+/*task22_1*/
+let a=1.5;
+let b=0.75;
+alert(a+b);
+/*task23_1*/
+a=-100;
+alert(a);
+/*task23_2*/
+a=666;
+alert(-a);
+/*task25_1*/
+a=13;
+b=5;
+alert(a%b);
+/*task26_1*/
+a=2;
+b=10;
+alert(a**b);
+/*task28_1*/
+let str="Shurygin";
+alert(str);
+/*task29_1*/
+str="!!!"
+alert(str);
+/*task29_2*/
+let stri="java";
+let strin="script";
+alert(stri+strin);
+/*task29_3*/
+let string="hello";
+let stringi="world";
+alert(string+stringi);
+/*task30_1*/
+let len="jnigdfijndfgdfgjnidfgjniedfgnijjnidfgjidfgnijndfgjniodfgjniodfgjinodfgijnoojnik";
+alert(len.length);
+/*task31_1*/
+let str1='xxx';
+let str2='yyy';
+let txt='aaa '+str1+' bbb '+str2+' ccc';
+alert(txt);
+/*task34_1*/
+let ab;
+alert(ab);
+/*task35_1*/
+let bc=null;
+alert(bc);
+/*task36_1*/
+let t=true;
+alert(t);
+/*task36_2*/
+let f=false;
+alert(f);
+/*task37_1*/
+let x='abc';
+let y='def';
+alert(x*y);
+/*task38_1*/
+alert(10/0);
+/*task38_2*/
+alert(-10/0);
+/*task39_1*/
+let num1=123;
+console.log(num1);
+/*task39_2*/
+num1=123;
+let num2=456;
+let num3=789;
+console.log(num1,num2,num3);
+/*task40_1*/
+let st='abc';
+let s=123;
+let m=12.5;
+let h=true;
+console.log(s);
+console.log(m);
+console.log(h);
+/*task41_1*/
+//alert(eee);
+/*task42_1*/
+const PI=3.14
+let r=4;
+alert(PI*r**2);
+//task45_1
+let u='2';
+let e='3';
+alert(+u+ +e);
+//task47_1
+let p=parseInt('5px');
+let xx=parseInt('6px');
+alert(p+xx);
+//task47_2
+p=parseFloat('5.5px');
+xx=parseFloat('6.25px');
+alert(p+xx);
+//task48_1
+r=1;
+u=2;
+alert(String(r)+String(u));
+//task49_1
+let n=123;
+alert(String(n).length);
+//task49_2
+n=123;
+u=2;
+alert(String(n).length+ String(u).length);
+//task52_1
+let z='abcde';
+alert(z[0]+z[2]+z[4]);
+//task52_2
+z='abcde';
+let v=z[4]+z[3]+z[2]+z[1]+z[0];
+alert(v);
+//task52_3
+z='abcdef';
+v=4;
+alert(z[v]);
---
+++ script.js
@@ -0,0 +1,38 @@
+"use strict";//strogiy rejim
+let num='Конец проги';
+let a;
+a=10
+alert(a)
+a=20;
+alert(a);
+a=1+2+3;
+alert(a);
+/*task18_1*/
+a=10;
+let b=2;
+alert(a+b);
+
+alert(a-b);
+
+alert(a*b);
+
+alert(a/b);
+/*task18_2*/
+let c=10;
+let d=5;
+let result=c+d;
+alert(result);
+/*task18_3*/
+a=1;
+b=2;
+c=3;
+alert(a+b+c);
+/*task18_4*/
+a=10;
+b=5;
+c=a-b;
+d=7;
+result=c+d;
+alert(result)
+alert(num);/*alert*/
+
