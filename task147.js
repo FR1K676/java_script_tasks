dec7ac14b66cb8ca1447f748ec49c26d31d36518
FR1K676 <152060296+FR1K676@users.noreply.github.com>
2024-11-04 16:53:17 +0300

    Add files via upload

---
+++ task147.js
@@ -0,0 +1,10 @@
+//task147
+for(let task147_1; task147_1<=100;task147_1++){
+    console.log(task147_1)
+}
+for(let task147_2=11;task147_2<=33;task147_2++){
+    console.log(task147_2)
+}
+for(let task147_3=0;task147_3<=100;task147_3+2){
+    console.log(task147_3)
+}
