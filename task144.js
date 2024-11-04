82a491354e13b5b493d4747e748eecf7177799bd
FR1K676 <152060296+FR1K676@users.noreply.github.com>
2024-11-04 16:52:57 +0300

    Add files via upload

---
+++ task144.js
@@ -0,0 +1,27 @@
+//task144
+let task144=['a','b','c','d','e']
+for(let elemtask144 of task144){
+    console.log(elemtask144)
+}
+//task145
+let task145={x:1,y:2,z:3}
+for(let key145 in task145){
+    console.log(key145)
+    console.log(task145[key145])
+}
+//task146
+let task146_1=0
+while(task146_1<=100){
+    console.log(task146_1)
+    task146_1++
+}
+let task146_2=11;
+while(task146_2<=33){
+    console.log(task146_2)
+    task146_2++
+}
+let task146_3=5
+while(task146_3<1000){
+    console.log(task146_3)
+    task146_3=task146_3+task146_3+task146_3
+}
