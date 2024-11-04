36a0b0fab98e7ed6dc2593b1206cd778964b02ad
FR1K676 <152060296+FR1K676@users.noreply.github.com>
2024-11-04 16:52:35 +0300

    Add files via upload

---
+++ task123.js
@@ -0,0 +1,14 @@
+'use strict'
+//task123
+let task1231 = true;
+let task1232 = true;
+
+if (task1231&&task1232) { 
+	console.log('+++');
+} else {
+	console.log('---');
+}
+let task124=10
+if(task124==10){
+    console.log('yes')
+}
